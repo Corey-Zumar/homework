6206f5cc85e5b65d93664f4e422ea3fb88405b81
Corey Zumar <czumar@berkeley.edu>
2017-09-11 21:57:19 -0700

    Updates

--- hw1_soln/BC/hyperparam_experiments/plot_returns.py
+++ hw1_soln/BC/hyperparam_experiments/plot_returns.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
 	epochs = range(len(returns_data))
 	returns_means = [item['mean'] for item in returns_data]
 
-	plt.xticks(np.arange(0, len(epochs), 1.0))
+	plt.xticks(np.arange(1, len(epochs) + 1, 1.0))
 	plt.title("Mean return in the Ants environment for a neural network \n as a function of the number of training epochs")
 	plt.xlabel("Number of training epochs")
 	plt.ylabel("Mean return for 3 rollouts")
